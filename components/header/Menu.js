edd3156f410223f92715737cd787bf4efb4e1b7b
Carlos Loureda <carloslouredaparrado@gmail.com>
2021-05-03 19:03:07 +0200

    refactor: Removed console.log

--- components/header/Menu.js
+++ components/header/Menu.js
@@ -5,7 +5,6 @@ import Link from 'next/link';
 export const Menu = () => {
   const { primaryMenuData } = useGlobalData();
   const { nodes: items } = primaryMenuData?.data?.menu?.menuItems ?? [];
-  console.log("items: ", items)
   return (
     <nav className={clsx('hidden md:flex')}>
       {items?.map((item) => {
